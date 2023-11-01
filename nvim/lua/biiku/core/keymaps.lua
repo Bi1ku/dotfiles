0d24b418ff436a9af610f65f4268390fef5885c9
Owen Shi <biiku@owens-air.mynetworksettings.com>
2023-10-31 20:16:16 -0400

    leader key works

--- nvim/lua/biiku/core/keymaps.lua
+++ nvim/lua/biiku/core/keymaps.lua
@@ -1,12 +1,13 @@
-vim.g.mapleader = "<Space>"
+vim.g.mapleader = " "
 
 -- vim keymaps
 vim.keymap.set("i", "kj", "<ESC>")
 vim.keymap.set("n", "<c-s>", ":w <CR>")
+vim.keymap.set("n", "<tab>", ">>")
 
 -- telescope
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<Space><Space>", builtin.find_files, {})
+vim.keymap.set("n", "<Leader><Space>", builtin.find_files, {})
 
 -- nvim-tree
 vim.keymap.set("n", "<C-b>", ":NvimTreeFindFileToggle<CR>")
