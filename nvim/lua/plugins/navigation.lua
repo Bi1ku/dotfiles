449b22fbbb4eb69795e97034fd8e0933f3293bd2
Bi1ku <2008owenshi@gmail.com>
2024-07-28 18:29:20 -0400

    hardtime.lua

--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -1,26 +1,26 @@
 return {
-  {
-    "nvim-tree/nvim-tree.lua",
-    name = "tree",
-    version = "*",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    opts = {},
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		name = "nvim-tree",
+		version = "*",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {},
+	},
 
-  {
-    "nvim-telescope/telescope.nvim",
-    name = "telescope",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup()
+	{
+		"nvim-telescope/telescope.nvim",
+		name = "telescope",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup()
 
-      local builtin = require("telescope.builtin")
+			local builtin = require("telescope.builtin")
 
-      vim.keymap.set("n", "<Leader>g", builtin.live_grep)
-      vim.keymap.set("n", "<Leader><Space>", builtin.find_files)
-    end,
-  },
+			vim.keymap.set("n", "<Leader>g", builtin.live_grep)
+			vim.keymap.set("n", "<Leader><Space>", builtin.find_files)
+		end,
+	},
 }
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -23,6 +23,7 @@ return {
 
 	{
 		"nvim-lualine/lualine.nvim",
+		name = "lualine",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
 }
