ce3b7e26b29d8973265f1a4b4757499aa579c612
Bi1ku <2008owenshi@gmail.com>
2024-08-30 22:48:46 -0400

    remove some not needed plugins and add cowboy

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,84 +1,94 @@
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    name = "copilot",
-    event = "InsertEnter",
-    opts = {
-      suggestion = {
-        auto_trigger = true,
-        keymap = {
-          accept = "<C-e>",
-        },
-      },
-    },
-  },
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		name = "copilot",
+		event = "InsertEnter",
+		opts = {
+			suggestion = {
+				enable = false,
+				auto_trigger = true,
+				keymap = {
+					accept = "<C-e>",
+				},
+			},
+		},
+	},
 
-  {
-    "m4xshen/hardtime.nvim",
-    name = "hardtime",
-    event = "InsertEnter",
-    lazy = true,
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    opts = {
-      max_count = 5,
-    },
-  },
+	{
+		dir = "~/.config/nvim/lua/custom/cowboy.nvim",
+		opts = {
+			keys = { "h", "j", "k", "l" },
+			thresh = 5,
+			delay = 1000,
+		},
+	},
 
-  {
-    "windwp/nvim-ts-autotag",
-    name = "autotag",
-    lazy = true,
-    event = {
-      "BufReadPre",
-      "BufNewFile",
-    },
-    opts = {},
-  },
+	--  {
+	--    "m4xshen/hardtime.nvim",
+	--    name = "hardtime",
+	--    event = "InsertEnter",
+	--    lazy = true,
+	--    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+	--    opts = {
+	--      max_count = 5,
+	--    },
+	--  },
 
-  {
-    "windwp/nvim-autopairs",
-    event = {
-      "BufReadPre",
-      "BufNewFile",
-    },
-    lazy = true,
-    name = "autopairs",
-    opts = {},
-    config = true,
-  },
+	--  {
+	--    "windwp/nvim-ts-autotag",
+	--    name = "autotag",
+	--    lazy = true,
+	--    event = {
+	--      "BufReadPre",
+	--      "BufNewFile",
+	--    },
+	--    opts = {},
+	--  },
+	--
+	--  {
+	--    "windwp/nvim-autopairs",
+	--    event = {
+	--      "BufReadPre",
+	--      "BufNewFile",
+	--    },
+	--    lazy = true,
+	--    name = "autopairs",
+	--    opts = {},
+	--    config = true,
+	--  },
 
-  {
-    "nvimtools/none-ls.nvim",
-    name = "null-ls",
-    lazy = false,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      local null_ls = require("null-ls")
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	{
+		"nvimtools/none-ls.nvim",
+		name = "null-ls",
+		lazy = false,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			local null_ls = require("null-ls")
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.formatting.prettier,
-          null_ls.builtins.formatting.clang_format,
-        },
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.formatting.clang_format,
+				},
 
-        on_attach = function(client, bufnr)
-          if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({ async = false })
-              end,
-            })
-          end
-        end,
-      })
-    end,
-  },
+				on_attach = function(client, bufnr)
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								vim.lsp.buf.format({ async = false })
+							end,
+						})
+					end
+				end,
+			})
+		end,
+	},
 }
